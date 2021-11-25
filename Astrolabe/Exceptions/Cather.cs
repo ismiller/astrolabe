fff1216edea46ce142d17984adf07bebf5aa23ec
Ivan Miller <i.s.miller@ya.ru>
2021-11-25 23:28:54 +0700

    Добавлен класс перехвата исключений

---
+++ Astrolabe/Exceptions/Cather.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using Astrolabe.Exceptions.Verifications;
+
+;
+
+namespace Astrolabe.Exceptions
+{
+    public static class Cather
+    {
+        public static void TryInvoke<TException, TArg>(this Action<TArg> action, TArg arg) where TException : Exception
+        {
+            try
+            {
+                Argument.NotNull(action, nameof(action));
+                action.Invoke(arg);
+            }
+            catch (TException e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+        }
+    }
+}
