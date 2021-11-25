4f918e3413369d83f8ce2813150dd835759a088b
Ivan Miller <i.s.miller@ya.ru>
2021-11-25 23:27:01 +0700

    Добавлен класс проверки аргументов

---
+++ Astrolabe/Exceptions/Verifications/Argument.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Astrolabe.Exceptions.Verifications
+{
+    public static class Argument
+    {
+        public static void NotNull<T>(T value, string name) where T : class
+        {
+            if (value is null)
+            {
+                throw new ArgumentNullException(name);
+            }
+        }
+
+        public static void NotNullOrWhiteSpace(string value, string name)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException(string.Format(CultureInfo.CurrentCulture, "String cannot be empty", name));
+            }
+        }
+    }
+}
