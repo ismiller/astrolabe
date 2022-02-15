afc29bfb91bbd24614c7d774e7c5a3cdedb1152c
Ivan Miller <ivan.miller@myworkflow.ru>
2022-02-15 19:52:48 +0700

    Добавлен тестовый проект

---
+++ src/Test/TestPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Astrolabe.AppSDK.Controls;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Test
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class TestPage : AstrolabePage
+    {
+        public TestPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ src/Test/TestPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Astrolabe.Core.ViewModels.Abstractions;
+
+namespace Test
+{
+    public class TestPageViewModel : INavigatable
+    {
+        public void Prepare(INavigationArgs args)
+        {
+            
+        }
+
+        public void SetObserver(IViewObserver observer)
+        {
+
+        }
+    }
+}
